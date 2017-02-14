1fa4c14c539030a70a99f3ffa7e3011748f85a3e
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-02-14 17:33:59 +0100

    Use alertDialog

--- app/src/main/java/com/example/bartek/androidtutorial/MainActivity.java
+++ app/src/main/java/com/example/bartek/androidtutorial/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.bartek.androidtutorial;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.StringRes;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,15 +18,29 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
            }
 
-    public void btnClick(View view) {
-        Intent intent = new Intent(view.getContext(), SecondActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit);
-        String string = String.valueOf(editText.getText());
+    public void btnClick(final View view) {
+        AlertDialog.Builder alertD = new AlertDialog.Builder(this);
+        alertD.setMessage("Are you sure? ");
+        alertD.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(view.getContext(), SecondActivity.class);
+                EditText editText = (EditText) findViewById(R.id.edit);
+                String string = String.valueOf(editText.getText());
 
-        intent.putExtra(SecondActivity.KEY, string);
+                intent.putExtra(SecondActivity.KEY, string);
 
-        startActivity(intent);
+                startActivity(intent);
 
+            }
+        });
+        alertD.setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertD.show();
     }
 }
 
