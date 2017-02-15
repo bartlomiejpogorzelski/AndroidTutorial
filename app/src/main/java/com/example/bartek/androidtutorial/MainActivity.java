cf7ec6b6cb8dd7c6590d2bb7444fd08fb27670da
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-02-15 18:14:30 +0100

    Use simple notyfication

--- app/src/main/java/com/example/bartek/androidtutorial/MainActivity.java
+++ app/src/main/java/com/example/bartek/androidtutorial/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bartek.androidtutorial;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.StringRes;
@@ -41,6 +44,24 @@ public class MainActivity extends Activity{
             }
         });
         alertD.show();
+    }
+
+    public void nClick(View view) {
+        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent= new Intent(this, SecondActivity.class);
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,0 );
+
+        Notification.Builder notificationBuilder= new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_menu_zoom)
+                .setContentTitle("NOTYFIKACJA")
+                .setContentText("This is content ...")
+                .setContentIntent(pendingIntent);
+
+        Notification notification = notificationBuilder.build();
+
+        notificationManager.notify(0, notification);
+
     }
 }
 
--- app/src/main/java/com/example/bartek/androidtutorial/SecondActivity.java
+++ app/src/main/java/com/example/bartek/androidtutorial/SecondActivity.java
@@ -12,14 +12,14 @@ public static final String KEY= "key";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        Bundle bundle= getIntent().getExtras();
-        String text = bundle.getString(KEY, "");
-        TextView textView= (TextView) findViewById(R.id.text);
-
-
-
-        textView.setText(text);
+//
+//        Bundle bundle= getIntent().getExtras();
+//        String text = bundle.getString(KEY, "");
+//        TextView textView= (TextView) findViewById(R.id.text);
+//
+//
+//
+//        textView.setText(text);
 
     }
 }
