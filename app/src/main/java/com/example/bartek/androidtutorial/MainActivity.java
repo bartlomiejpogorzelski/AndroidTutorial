28a5fd37fdaf7d24151b13d071308857a993cbc7
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-02-22 18:48:37 +0100

    Use sharedpreferences

--- app/src/main/java/com/example/bartek/androidtutorial/MainActivity.java
+++ app/src/main/java/com/example/bartek/androidtutorial/MainActivity.java
@@ -6,19 +6,31 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity{
     private int i=0;
+    int count=0;
+    public final static String PREF= "my prefs";
+    public final static String KEY_PREF= " shared";
+    TextView sText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sText= (TextView) findViewById(R.id.sharedID);
+        SharedPreferences shP= getSharedPreferences(PREF,0);
+        count= shP.getInt(KEY_PREF, 0);
+        updateText(sText);
            }
 
     public void btnClick(final View view) {
@@ -67,5 +79,25 @@ public class MainActivity extends Activity{
         notificationManager.notify(0, notification);
 
     }
+
+
+    public void sherClick(View view) {
+        count++;
+
+        updateText(sText);
+    }
+    private void updateText(TextView sText) {
+        sText.setText(String.valueOf(count));
+    }
+
+    @Override
+    protected void onDestroy() {    //onDestroy pojawia sie w momencie zamknieta aktywnosci
+        super.onDestroy();
+        SharedPreferences shP = getSharedPreferences(PREF,0);
+        shP.edit().putInt(KEY_PREF, count).apply();
+
+    }
+
+
 }
 
